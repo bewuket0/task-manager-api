aa162c11be31eeeb240c7d42975b594b694a51ee
b0 <bewuket1621@gmail.com>
2024-03-28 21:16:38 +0300

    create online db on atlas

--- config/db.js
+++ config/db.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.DATABASE_LOCAL);
+    // const conn = await mongoose.connect(process.env.DATABASE_LOCAL);
+    const conn = await mongoose.connect(process.env.DATABASE_ONLINE);
     console.log(`Mongo database connected : ${conn.connection.host}`);
   } catch (error) {
     console.error("error : ", error.message);
